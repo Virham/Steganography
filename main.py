8c93a8efc4e1bfec5eb1f7c2ace00fb0ebba0ebb
Virham <71260381+Virham@users.noreply.github.com>
2024-05-21 11:09:08 +0200

    can encode any binary

--- main.py
+++ main.py
@@ -29,11 +29,26 @@ def encode_string(string):
         d = [0] * (8 - len(d)) + d
         data.extend(d)
     
-    return data + [0] * 8 # end of file byte
+    return data
 
+def encode_file(binary):
+    data = []
+    length = [int(b) for b in bin(len(binary))[2:]]
+    length = [0] * (32 - len(length)) + length
+    data.extend(length)
+    
+    for b in binary:
+        d = [int(bit) for bit in bin(b)[2:]]
+        d = [0] * (8 - len(d)) + d
+        if len(d) > 8:
+            continue
+        data.extend(d)
+    return data
 
-def encode_image(img : Image, string) -> Image:
-    data = encode_string(string)
+def encode_image(img : Image, file) -> Image:
+    with open(file, "rb") as f:
+        data = encode_file(f.read())
+
     size = get_min_img_size(img, data)
     image = img.resize(size)
 
@@ -57,10 +72,17 @@ def encode_image(img : Image, string) -> Image:
     return image
 
 
+def construct_length(size_bytes):
+    size_bytes = size_bytes[::-1]
+    return sum([size_bytes[i] * 256 ** i for i in range(len(size_bytes))])
+
 def decode_image(image : Image):
     pixels = image.load()
     data = []
     byte = []
+    
+    size = 0
+    size_bytes = []
     
     for j in range(image.height):
         for i in range(image.width):
@@ -68,22 +90,26 @@ def decode_image(image : Image):
             if len(byte) >= 8:
                 d = "0b" + "".join([str(b) for b in byte[0:8]])
                 byte = byte[8:]
+                
+                decimal = int(d, 2)
+                
+                if not size:
+                    size_bytes.append(decimal)
+                    if len(size_bytes) == 4:
+                        size = construct_length(size_bytes)
+                    continue
 
-                ascii = int(d, 2)
-                if ascii < 10:
-                    return "".join(data)
+                data.append(decimal)
+                if len(data) >= size:
+                    return bytes(data)
 
-                data.append(chr(ascii))
-
-    return "".join(data)
-
-
+    return bytes(data)
 
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("ok")
+        print("bad arguments.")
         quit()
 
     if sys.argv[1] == "encode":
@@ -92,19 +118,18 @@ if __name__ == "__main__":
             quit()
 
         img = Image.open(sys.argv[2])
-        with open(sys.argv[3], "r") as f:
-            text = f.read()
-        
+        file = sys.argv[3]
         output = sys.argv[4]
-        encode_image(img, text).save(output)
+        encode_image(img, file).save(output)
 
     elif sys.argv[1] == "decode":
-        if len(sys.argv) < 3:
+        if len(sys.argv) < 4:
             print("not enough arguments.")
             quit()
         
-        print(decode_image(Image.open(sys.argv[2])))
+        with open(sys.argv[3], "wb") as f:
+            f.write(decode_image(Image.open(sys.argv[2])))
 
     else:
-        print("You dyslexic")
+        print("you're dyslexic.")
 
